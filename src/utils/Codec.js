80863d80be1182dda290e3b8a4327877910855a1
Bumble <63314839+Kariem816@users.noreply.github.com>
2022-12-06 23:30:14 +0200

    Adding more encoding

--- src/utils/Codec.js
+++ src/utils/Codec.js
@@ -1,9 +1,12 @@
-const { encode: encodeBase64, decode: decodeBase64 } = require('./Base64');
+const { encode: encodeBase64, decode: decodeBase64 } = require('./encoding/Base64');
+const { encode: encodeURL, decode: decodeURL } = require('./encoding/URL');
 
 function encode(data, encoding = 'base64') {
     switch (encoding.toLowerCase()) {
         case 'base64':
             return encodeBase64(data);
+        case 'url':
+            return encodeURL(data);
     }
 }
 
@@ -11,6 +14,8 @@ function decode(data, encoding = 'base64') {
     switch (encoding.toLowerCase()) {
         case 'base64':
             return decodeBase64(data);
+        case 'url':
+            return decodeURL(data);
     }
 }
 
